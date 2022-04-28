59f7e6a968b50a4c2a731aa468e4583733061f8f
Kodeekk <kot.tkachuk@gmail.com>
2022-04-28 21:37:08 +0300

    Bugs fixes

--- src/main/java/com/vsleepvote/vsleepvote/VSleepVote.kt
+++ src/main/java/com/vsleepvote/vsleepvote/VSleepVote.kt
@@ -1,12 +1,16 @@
 package com.vsleepvote.vsleepvote
 
 import com.vsleepvote.vsleepvote.commands.vsleepvote
+import lombok.Getter
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class VSleepVote : JavaPlugin() {
+    @Getter
+    var configFile = File(dataFolder, "config.yml")
     override fun onEnable() {
-
+        Bukkit.getConsoleSender().sendMessage("$configFile \n $config")
         getCommand("vsleepvote")?.setExecutor(vsleepvote)
         Bukkit.getConsoleSender().sendMessage("vSleep Vote started!")
         // Plugin startup logic
--- src/main/java/com/vsleepvote/vsleepvote/commands/vsleepvote.kt
+++ src/main/java/com/vsleepvote/vsleepvote/commands/vsleepvote.kt
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender
 object vsleepvote : CommandExecutor{
     var votedPlayers = mutableListOf<String>("", "")
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if(args.size > 1) {
+        if(args.size > 1 || args.size == 0) {
             sender.sendMessage("Invalid syntax!")
             return true
         }
@@ -16,7 +16,7 @@ object vsleepvote : CommandExecutor{
             votedPlayers.add(sender.name)
             Bukkit.getPlayer(sender.name)?.sendMessage("${Bukkit.getPlayer(sender.name)?.name} vote to skip night!")
 
-            if(votedPlayers.size > 5 || Bukkit.getOnlinePlayers().size < 5)
+            if(votedPlayers.size > 5 || Bukkit.getOnlinePlayers().size < 2)
                 Bukkit.getWorld("world")?.time = 1500
                 sender.sendMessage("Night skipped!")
         }
